f3c8393530fbb4b4183459bf77e1e261098d0b10
riki karasawa <fabulous.riki@gmail.com>
2021-05-08 23:44:16 +0900

    投稿ページ仕上げ

--- app/public/wp-content/themes/myblog/includes/header.php
+++ app/public/wp-content/themes/myblog/includes/header.php
@@ -1,3 +1,6 @@
+<!-- // プラグイン差し込み用テンプレートタグ -->
+<?php wp_body_open(); ?>
+
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
         <a class="navbar-brand" href="index.html">Start Bootstrap</a>
