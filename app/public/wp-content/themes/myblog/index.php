909ba4455d0c49a0eecd4f245a36fa12a7ac7910
riki karasawa <fabulous.riki@gmail.com>
2021-05-17 09:58:24 +0900

    ページネーション実装

--- app/public/wp-content/themes/myblog/index.php
+++ app/public/wp-content/themes/myblog/index.php
@@ -54,7 +54,22 @@
 
           <!-- Pager -->
           <div class="clearfix">
-            <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+            <!-- ページネーション：新しい記事へ -->
+            <?php
+            $link = get_previous_posts_link('&larr; 新しい記事へ');
+            if($link) {
+              $link = str_replace('<a', '<a class="btn btn-primary float-left"', $link);
+              echo $link;
+            }
+            ?>
+            <!-- ページネーション：古い記事へ -->
+            <?php
+            $link = get_next_posts_link('古い記事へ &rarr;');
+            if($link) {
+              $link = str_replace('<a', '<a class="btn btn-primary float-right"', $link);
+              echo $link;
+            }
+            ?>
           </div>
         <?php else : ?>
           <p>記事が見つかりませんでした</p>
